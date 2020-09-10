d81a55149331f724a4c276c2810a7098b41c1656
Krishnam rathi <krishnam.rathi@gmail.com>
2020-09-10 18:36:48 +0530

    Updates

---
+++ precache-manifest.894caba976239d3d28ff0d6be4509bc1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a827eb401e4683356c530c41da240c19",
+    "url": "/Grocery-frontend/index.html"
+  },
+  {
+    "revision": "50bac5b5e8c7b8c7c3d6",
+    "url": "/Grocery-frontend/static/css/main.f0af9c87.chunk.css"
+  },
+  {
+    "revision": "390e9c6c75059c3f7e54",
+    "url": "/Grocery-frontend/static/js/2.0da36431.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Grocery-frontend/static/js/2.0da36431.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50bac5b5e8c7b8c7c3d6",
+    "url": "/Grocery-frontend/static/js/main.3237f7e8.chunk.js"
+  },
+  {
+    "revision": "7f7a81372b1f102e8efe",
+    "url": "/Grocery-frontend/static/js/runtime-main.9efd1b9d.js"
+  },
+  {
+    "revision": "800f47f796841b7c2fcfe02f24de61fe",
+    "url": "/Grocery-frontend/static/media/BannerIcons.800f47f7.png"
+  },
+  {
+    "revision": "c24b8be856427af60690dd9308212e38",
+    "url": "/Grocery-frontend/static/media/employee.c24b8be8.png"
+  },
+  {
+    "revision": "20dc4b2890784558a85fa3349dea42b3",
+    "url": "/Grocery-frontend/static/media/illu.20dc4b28.png"
+  },
+  {
+    "revision": "31a6a246fcf39cb48362ecc20a1881cb",
+    "url": "/Grocery-frontend/static/media/step1.31a6a246.png"
+  },
+  {
+    "revision": "07d5acfad767e204b1f03d7f4dfb5eb5",
+    "url": "/Grocery-frontend/static/media/step2.07d5acfa.png"
+  },
+  {
+    "revision": "08752df366a789528983055d54835c74",
+    "url": "/Grocery-frontend/static/media/step3.08752df3.png"
+  }
+]);
